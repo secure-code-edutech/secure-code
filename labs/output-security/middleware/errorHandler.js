874bae138f608ef8815d2bd2ef0e2749003a084e
yeremia.juan <yeremiajxt@gmail.com>
2025-11-05 10:59:25 +0700

    fix: error handling

--- labs/output-security/middleware/errorHandler.js
+++ labs/output-security/middleware/errorHandler.js
@@ -1,4 +1,4 @@
 module.exports = (err, req, res, next) => {
   // VULNERABLE: sending stack to client
-  res.status(500).send(`<pre>${err.stack}</pre>`);
+  res.status(500).send(`<pre>Error bang...</pre>`);
 };
