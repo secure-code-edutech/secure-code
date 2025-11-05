2097d050fa3eaa48b26e0cae9fe1616ce1a088c9
Rhesa Daiva Bremana <rhesa.daiva@gmail.com>
2025-11-05 10:17:19 +0700

    fix(output-security): secure output and error handling

--- labs/output-security/middleware/errorHandler.js
+++ labs/output-security/middleware/errorHandler.js
@@ -1,4 +1,19 @@
+const crypto = require("crypto");
+
 module.exports = (err, req, res, next) => {
-  // VULNERABLE: sending stack to client
-  res.status(500).send(`<pre>${err.stack}</pre>`);
+    // VULNERABLE: sending stack to client
+    // res.status(500).send(`<pre>${err.stack}</pre>`);
+    const token = crypto.randomBytes(16).toString("hex");
+    res.status(500).send(
+        `<h1>Oops, an error occured, please call our System Support</h1><br /><span>ID: ${token}</span><br/><span>Access time: ${
+            new Date().toString()
+        }
+        }</span>`,
+    );
+
+    console.log(
+        `An error occured on errorHandler \nID: ${token} \nStack:${
+            JSON.stringify(err.stack)
+        }\n${new Date().toString()} `,
+    );
 };
